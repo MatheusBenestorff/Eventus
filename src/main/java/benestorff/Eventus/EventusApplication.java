ca7e586574cb6d590f00bb7499aee22076f3ab6a
Matheus Benestorff <matheusbenestorff@gmail.com>
2025-05-11 23:18:17 -0300

    Substituindo versão antiga JSP por nova com Spring Boot

---
+++ src/main/java/benestorff/Eventus/EventusApplication.java
@@ -0,0 +1,13 @@
+package benestorff.Eventus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventusApplication.class, args);
+	}
+
+}
